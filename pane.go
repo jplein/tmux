30e9ccfe09eb3e5916b10d280bb136bcf1886b59
Jason Plein <jplein@gmail.com>
2025-02-25 10:09:39 -0800

    Adding comment

--- pane.go
+++ pane.go
@@ -10,6 +10,7 @@ func (r *Runner) SetPaneWidth(pane string, width int) error {
 	return err
 }
 
+// Get the active pane, as a string like "%14".
 func (r *Runner) GetActivePane() (string, error) {
 	return r.Run("list-panes -F '#{pane_id}' -f '#{m:#{pane_active},1}'")
 }
